f58e97cc9e83cb0575b204b376dc9053390aceea
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-11-19 18:57:25 -0300

    feat(repository): implement Repository pattern for Posts – part 2

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -1,13 +1,40 @@
+import { resolve } from "path";
 import { PostRepository } from "./post-repository";
+import { readFile } from "fs/promises";
+import { PostModel } from "@/models/post/post-model";
 
 const ROOT_DIR = process.cwd();
+const JSON_POSTS_FILE_PATH = resolve(
+  ROOT_DIR,
+  "src",
+  "db",
+  "seed",
+  "posts.json"
+);
 
 export class JsonPostRepository implements PostRepository {
-  private async readFromDisk() {}
+  private async readFromDisk(): Promise<PostModel[]> {
+    const jsonContent = await readFile(JSON_POSTS_FILE_PATH, "utf-8");
+    const parsedJson = JSON.parse(jsonContent);
+    const { posts } = parsedJson;
+    return posts;
+  }
 
-  async findAll(): Promise<PostModel[]> {}
+  async findAll(): Promise<PostModel[]> {
+    return this.readFromDisk();
+  }
+  async findById(id: string): Promise<PostModel> {
+    const posts = await this.findAll();
+    const post = posts.find((post) => post.id === id);
+
+    if (!post) throw new Error("Post não encontrado");
+
+    return post;
+  }
+
+  async blabla() {
+    //
+  }
 }
 
-export const postRepository = new JsonPostRepository();
-
-console.log(ROOT_DIR);
+export const postRepository: PostRepository = new JsonPostRepository();
