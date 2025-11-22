6f5a18d468fb29945d6059cef19f2f5745a2e769
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-11-22 18:07:39 -0300

    feat(ui): add Container component and Tailwind CSS tips

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,11 @@
+import { Container } from "@/components/Container";
 import { PostsList } from "@/components/PostsList";
 import { SpinLoader } from "@/components/SpinLoader";
-import clsx from "clsx";
 import { Suspense } from "react";
 
 export default async function HomePage() {
   return (
-    <div
-      className={clsx(
-        "text-slate-900",
-        "bg-slate-100",
-        "min-h-screen",
-        "dark:bg-slate-900",
-        "dark:text-slate-100"
-      )}
-    >
+    <Container>
       <header>
         <h1 className="text-6xl font-bold text-center py-8">Aqui é a HEADER</h1>
       </header>
@@ -25,6 +17,6 @@ export default async function HomePage() {
       <footer>
         <h1 className="text-6xl font-bold text-center py-8">FOOTER</h1>
       </footer>
-    </div>
+    </Container>
   );
 }
