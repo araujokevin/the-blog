ae61c0c39eea0d0b6665ef3971e2e3b9d2ac2e09
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-11-24 12:50:09 -0300

    feat(ui): finish FeaturedPost component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,18 +14,29 @@ export default async function HomePage() {
       <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
         <Link className="w-full h-full overflow-hidden rounded-xl" href="#">
           <Image
+            className="w-full h-full object-cover object-center group-hover:scale-105 transition duration-300"
             src="/images/bryen_0.png"
             width={1200}
             height={720}
             alt="Título do Post"
-            className="group-hover:scale-105 transition duration-300"
+            priority
           />
         </Link>
-        <div>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
-          ducimus dolorem rerum soluta voluptates? Veritatis illum fugit
-          reiciendis voluptatum perspiciatis esse nisi eveniet quod debitis?
-          Quidem perspiciatis id sint recusandae!
+        <div className="flex flex-col gap-4 sm:justify-center">
+          <time className="text-slate-600  text-sm/tight" dateTime="2025-04-20">
+            20/04/2025 10:00
+          </time>
+
+          <h1 className="text-2xl/tight font-extrabold sm:text-4xl">
+            <Link href="#">Lorem, ipsum dolor.</Link>
+          </h1>
+
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque
+            ducimus dolorem rerum soluta voluptates? Veritatis illum fugit
+            reiciendis voluptatum perspiciatis esse nisi eveniet quod debitis?
+            Quidem perspiciatis id sint recusandae!
+          </p>
         </div>
       </section>
 
