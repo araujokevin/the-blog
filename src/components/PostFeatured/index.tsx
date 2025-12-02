213d273f20f13c0118efece13ff8c3c6f26c99a8
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-12-01 21:05:03 -0300

    perf(cache): use React cache to prevent duplicate API or Repository calls

--- src/components/PostFeatured/index.tsx
+++ src/components/PostFeatured/index.tsx
@@ -1,10 +1,13 @@
 import { PostCoverImage } from "../PostCoverImage";
 
 import { PostSummary } from "../PostSummary";
+import { findAllPublicPost } from "@/lib/post/queries";
 
-export function PostFeatured() {
-  const slug = "qualquer";
-  const postLink = `/post/${slug}}`;
+export async function PostFeatured() {
+  const posts = await findAllPublicPost();
+  const post = posts[0];
+
+  const postLink = `/post/${post.slug}}`;
 
   return (
     <section className="grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2 group">
@@ -15,19 +18,17 @@ export function PostFeatured() {
         imageProps={{
           width: 1200,
           height: 720,
-          src: "/images/bryen_9.png",
-          alt: "Alt da imagem",
+          src: post.coverImageUrl,
+          alt: post.title,
           priority: true,
         }}
       />
       <PostSummary
         postLink={postLink}
         postHeading="h1"
-        title={"Rotina matinal de pessoas altamente eficazes"}
-        excerpt={
-          "O Next.js também é uma boa escolha para quem quer se preocupar com performance e SEO."
-        }
-        createdAt={"2025-04-08T00:24:38.616Z"}
+        title={post.title}
+        excerpt={post.excerpt}
+        createdAt={post.createdAt}
       />
     </section>
   );
