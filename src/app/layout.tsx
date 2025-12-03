d7771e4b19b4fc627e123e928914da7fbacbedd0
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-12-03 02:05:09 -0300

    feat(ui): separate Footer component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Container } from "@/components/Container";
 import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: {
@@ -23,9 +24,7 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
         <Container>
           <Header />
           {children}
-          <footer>
-            <h1 className="text-6xl font-bold text-center py-8">FOOTER</h1>
-          </footer>
+          <Footer />
         </Container>
       </body>
     </html>
