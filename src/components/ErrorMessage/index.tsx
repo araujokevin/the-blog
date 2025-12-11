459e04846e90b98bd7e76650deaa5ab7854fc171
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-12-11 08:40:54 -0300

    feat(ui): add ErrorMessage component for client-side error display

---
+++ src/components/ErrorMessage/index.tsx
@@ -0,0 +1,29 @@
+import clsx from "clsx";
+
+type ErrorMessageProps = {
+  contentTitle: string;
+  content: React.ReactNode;
+};
+
+export default function ErrorMessage({
+  contentTitle,
+  content,
+}: ErrorMessageProps) {
+  return (
+    <>
+      <div
+        className={clsx(
+          "min-h-80 bg-slate-900 text-slate-100",
+          "mb-16 p-8 rounded-xl",
+          "flex items-center justify-center",
+          "text-center"
+        )}
+      >
+        <div>
+          <h1 className="text-7xl/tight mb-4 font-extrabold">{contentTitle}</h1>
+          <p>{content}</p>
+        </div>
+      </div>
+    </>
+  );
+}
