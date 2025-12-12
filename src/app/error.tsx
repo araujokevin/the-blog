218a82de05fb84cd0137c56fa69c26943c5034de
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2025-12-12 08:12:55 -0300

    feat(error-handling): add Error Boundaries using error.tsx files

---
+++ src/app/error.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import ErrorMessage from "@/components/ErrorMessage";
+import { useEffect } from "react";
+
+export const metadata = {
+  title: "Internal Server Error",
+};
+
+type RootErrorPageProps = {
+  error: Error;
+  reset: () => void;
+};
+
+export default function RootErrorPage({ error }: RootErrorPageProps) {
+  useEffect(() => {
+    console.log(error);
+  }, [error]);
+
+  return (
+    <ErrorMessage
+      contentTitle="501"
+      content="Ocorreu um erro do qual nossa aplicação não conseguiu se recuperar. Tente novamente mais tarde."
+    />
+  );
+}
