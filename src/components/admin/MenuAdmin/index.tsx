e401abe700b2adae858d469dc25c0b835a470b87
Kevin Araújo <177760731+araujokevin@users.noreply.github.com>
2026-01-25 13:18:43 -0300

    style(admin): style AdminMenu using Tailwind CSS

--- src/components/admin/MenuAdmin/index.tsx
+++ src/components/admin/MenuAdmin/index.tsx
@@ -1,12 +1,52 @@
+import clsx from "clsx";
+import { FileTextIcon, HouseIcon } from "lucide-react";
 import Link from "next/link";
 
 export function MenuAdmin() {
+  const navClasses = clsx(
+    "bg-slate-900 text-slate-100 rounded-lg",
+    "flex flex-col",
+    "sm:flex-row sm:flex-wrap",
+    // 'overflow-hidden',
+    // "h-10",
+  );
+  const linkClasses = clsx(
+    "[&>svg]:w-4 [&>svg]:h-4 px-4",
+    "flex items-center gap-2",
+    "transition hover:bg-slate-800 rounded-lg",
+    "h-10",
+    "shrink-0",
+  );
   return (
-    <nav>
-      <a href="/" target="_blank">
+    <nav className={navClasses}>
+      <a className={linkClasses} href="/" target="_blank">
+        <HouseIcon />
         Home
       </a>
-      <Link href="/admin/post">Posts</Link>
+      <Link className={linkClasses} href="/admin/post">
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link className={linkClasses} href="/admin/post">
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link className={linkClasses} href="/admin/post">
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link className={linkClasses} href="/admin/post">
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link className={linkClasses} href="/admin/post">
+        <FileTextIcon />
+        Posts
+      </Link>
+      <Link className={linkClasses} href="/admin/post">
+        <FileTextIcon />
+        Posts
+      </Link>
     </nav>
   );
 }
